914673e86af22c98d7d3219c09f98615fa89e821
aDwCarrazzone <andrewcarrazzone@gmail.com>
2018-04-19 20:30:47 -0300

    envio dos exercicios

--- aula07/alunos/ra8100217/assets/js/idade.js
+++ aula07/alunos/ra8100217/assets/js/idade.js
@@ -1,10 +1,13 @@
 function CalcIdade()
 {
 var Nome = document.getElementById('Nome').value;
+var NomeMaiusculo = Nome.toUpperCase();
 var AnoAtual = document.getElementById('AnoAtual').value;
 var AnoNascimento = document.getElementById('AnoNasc').value;
 var Idade = AnoAtual - AnoNascimento;
-console.log(Nome)
-console.log(Idade)
-document.getElementById('Resultado').value = Nome + " você tem " + Idade +" anos."
+console.log(Nome);
+console.log(Idade);
+console.log(NomeMaiusculo);
+document.getElementById('Resultado').value = NomeMaiusculo + ", você tem " + Idade +" anos." +" E nasceu no ano " + AnoNascimento +".";
+document.querySelector(".resultdiv").innerHTML = NomeMaiusculo + ", você tem " + Idade +" anos." +" E nasceu no ano " + AnoNascimento +".";
 }
